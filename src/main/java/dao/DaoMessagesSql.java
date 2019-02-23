36f357dcc67c1e005f783a66ae78c017e5347a56
Maksym Shevchenko <maksym.shevchenko@icloud.com>
2019-02-23 10:25:03 +0200

    Todos for dao

--- src/main/java/dao/DaoMessagesSql.java
+++ src/main/java/dao/DaoMessagesSql.java
@@ -1,4 +1,35 @@
 package dao;
 
-public class DaoMessagesSql {
+import dto.Message;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class DaoMessagesSql implements Dao<Message> {
+
+    private Connection connection;
+    private int userId;
+
+    public DaoMessagesSql(Connection connection, int userId) {
+        this.connection = connection;
+        this.userId = userId;
+    }
+
+    public void add(Message item) {
+        //todo: implement sending message
+    }
+
+    public void remove(int id) {
+        throw new IllegalStateException("Method is not supplied by this implementation");
+    }
+
+    public Message get(int id) {
+        //todo: implement getting message by id
+        return null;
+    }
+
+    public List<Message> getAll() {
+        //todo: implement gettind all messages by user id
+        return null;
+    }
 }
