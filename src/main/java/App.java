6d4b7f09bdc5d2bf0d4812b16f0f8486c6432a04
Maksym Shevchenko <maksym.shevchenko@icloud.com>
2019-02-26 22:56:45 +0200

    port

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -40,12 +40,12 @@ public class App {
         handler.addFilter(new FilterHolder(new LoginFilter(connection)),"/login/*", EnumSet.of(DispatcherType.INCLUDE,DispatcherType.REQUEST));
         handlerCollection.setHandlers(new Handler[] {handler});
 
-        Server server = new Server(8080);
+        Server server = new Server(5000);
 
         server.setHandler(handler);
 
         server.start();
-//        server.join();
+        server.join();
 
     }
 }
